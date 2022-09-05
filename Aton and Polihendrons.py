663a2d84f810861163a061dcc4aa96d7ee249023
yorkinbek <100893251+yorkinbek@users.noreply.github.com>
2022-09-05 15:01:08 +0500

    Some changes to the first solution

--- Aton and Polihendrons.py
+++ Aton and Polihendrons.py
@@ -1,16 +1,19 @@
 n = int(input())
-
-result = 0
+list = ["Tetrahedron", "Cube", "Octahedron", "Dodecahedron", "Icosahedron"]
+arr = []
+newList = []
 for i in range(0, n):
     str = input()
-    if(str[i] == "Tetrahedron"):
-        result += 4
-    if(str[i] == "Cube"):
-        result += 6
-    if(str[i] == "Octahedron"):
-        result += 8
-    if(str[i] == "Dodecahedron"):
-        result += 12
-    if(str[i] ==  "Icosahedron"):
-        result += 20
-    print(result)
+    newList.append(str)
+
+    if(newList[i]== list[0]):
+        arr.append(4)
+    if(newList[i] == list[1]):
+        arr.append(6)
+    if(newList[i]== list[2]):
+        arr.append(8)
+    if(newList[i] == list[3]):
+        arr.append(12)
+    if(newList[i] ==  list[4]):
+        arr.append(20)
+print(sum(arr))
