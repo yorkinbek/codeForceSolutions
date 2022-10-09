76dc2e1b7073d8289e49f967cbff07f18adc6536
yorkinbek <202090303@ajou.uz>
2022-10-09 17:04:51 +0500

    solutions in python

---
+++ CandidateandTwosister.py
@@ -0,0 +1,7 @@
+n = int(input())
+
+
+for i in range(n):
+    m= int(input())
+    result = int((m-1)/2)
+    print(result)
---
+++ FoxAndSnake.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+
+for i in range(1,n+1):
+    if (i%2==1):
+        print("#"*m)
+    elif (i%2==0 and i%4!=0):
+        print("."*(m-1)+"#")
+    elif (i%4==0):
+        print("#"+(m-1)*".")
+
